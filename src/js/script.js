4dbdaed2cb9fc9ac01e2284e18f2a226329e3e7f
y724u <68385107+y724u@users.noreply.github.com>
2021-01-28 11:50:13 +0900

    Revert "Revert "js環境構築""
    This reverts commit d2a9c69bf7f52c737a6a836bcc59a5eba7dee788.

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+  $('.test').css('color','red');
+});
+
+// swiper
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// wow.js
+new WOW().init();
