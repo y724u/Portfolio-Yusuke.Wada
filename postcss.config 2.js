8340c57479f608e3bb2e0740276580fed683ddca
y724u <68385107+y724u@users.noreply.github.com>
2021-01-29 11:35:09 +0900

    ロゴ追加

---
+++ postcss.config 2.js
@@ -0,0 +1,13 @@
+$ cat ./postcss.config.js
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }),
+    require('cssnano')({
+      preset: 'default',
+    })
+  ]
+}
+$
