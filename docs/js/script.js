0c0c05c1e67378e30ebe0f4075c382ed9ec0fb4d
y724u <68385107+y724u@users.noreply.github.com>
2021-02-02 18:50:21 +0900

    swiperレスポンシブを修正

--- docs/js/script.js
+++ docs/js/script.js
@@ -6,23 +6,29 @@ $(function () {
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   direction: 'horizontal',
+  // centeredSlides: true,
   loop: true,
-  slidesPerView: 2.5,
+  slidesPerView: 3,
   spaceBetween: 60,
 
   // // レスポンシブブレーポイント（画面幅による設定）
-  // breakpoints: {
-  //   // 画面幅が 640px 以上の場合（window width >= 640px）
-  //   640: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 5
-  //   },
-  //   // 画面幅が 980px 以上の場合（window width >= 980px）
-  //   980: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 10
-  //   }
-  // },
+  breakpoints: {
+    // 画面幅が 640px 以上の場合（window width >= 640px）
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // 画面幅が 1070px 以上の場合（window width >= 1070px）
+    1070: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    // 画面幅が 1290px 以上の場合（window width >= 1290px）
+    1290: {
+      slidesPerView: 2.5,
+      spaceBetween: 60,
+    }
+  },
 
   // If we need pagination
   pagination: {
