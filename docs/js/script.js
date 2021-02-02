fdef707104d1bf0aaea25f03c42b8304e0b70cd0
y724u <68385107+y724u@users.noreply.github.com>
2021-02-02 19:30:56 +0900

    位置を微調整

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,16 +1,8 @@
-$(function () {
-
-});
-
 // <!-- Slider  -->
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
   direction: 'horizontal',
-  // centeredSlides: true,
-  loop: true,
   slidesPerView: 3,
   spaceBetween: 60,
-
   // // レスポンシブブレーポイント（画面幅による設定）
   breakpoints: {
     // 画面幅が 640px 以上の場合（window width >= 640px）
@@ -21,16 +13,16 @@ const swiper = new Swiper('.swiper-container', {
     // 画面幅が 1070px 以上の場合（window width >= 1070px）
     1070: {
       slidesPerView: 2,
-      spaceBetween: 60,
+      spaceBetween: 40,
     },
     // 画面幅が 1290px 以上の場合（window width >= 1290px）
     1290: {
       slidesPerView: 2.5,
-      spaceBetween: 60,
+      spaceBetween: 45,
     }
   },
 
-  // If we need pagination
+  // pagination
   pagination: {
     el: '.swiper-pagination',
   },
@@ -39,10 +31,5 @@ const swiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
