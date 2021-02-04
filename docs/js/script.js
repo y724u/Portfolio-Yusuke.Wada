fb5d71d739386cb6215cadea4c00232c1fb11d22
y724u <68385107+y724u@users.noreply.github.com>
2021-02-04 14:52:53 +0900

    シングルクォーテーションに統一

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,10 +2,10 @@
 $(function () {
   $('a[href^="#"]').on('click', function () {
     let speed = 500;
-    let href = $(this).attr("href");
-    let target = $(href == "#" || href == "" ? 'html' : href);
+    let href = $(this).attr('href');
+    let target = $(href == '#' || href == '' ? 'html' : href);
     let position = target.offset().top -100;
-    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    $('html, body').animate({ scrollTop: position }, speed, 'swing');
     return false;
   });
 
