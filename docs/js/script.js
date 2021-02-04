779ed379dc58c81cb22924b656759761c3ab8423
y724u <68385107+y724u@users.noreply.github.com>
2021-02-04 14:31:33 +0900

    id場所をタイトルに変更

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,7 +4,7 @@ $(function () {
     let speed = 500;
     let href = $(this).attr("href");
     let target = $(href == "#" || href == "" ? 'html' : href);
-    let position = target.offset().top;
+    let position = target.offset().top -100;
     $("html, body").animate({ scrollTop: position }, speed, "swing");
     return false;
   });
