1ccb2e957ebcead4493c7dc3c6854a2e28c2a671
y724u <68385107+y724u@users.noreply.github.com>
2021-02-04 14:19:44 +0900

    スムーススクロールを追加

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,38 +1,50 @@
-// <!-- Slider  -->
-const swiper = new Swiper('.swiper-container', {
-  direction: 'horizontal',
-  slidesPerView: 3,
-  spaceBetween: 60,
-  // // レスポンシブブレーポイント（画面幅による設定）
-  breakpoints: {
-    // 画面幅が 640px 以上の場合（window width >= 640px）
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 30
+// <!-- スムーススクロール -->
+$(function () {
+  $('a[href^="#"]').on('click', function () {
+    let speed = 500;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+  // <!-- Slider  -->
+  const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 60,
+    // // レスポンシブブレーポイント（画面幅による設定）
+    breakpoints: {
+      // 画面幅が 640px 以上の場合（window width >= 640px）
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // 画面幅が 1070px 以上の場合（window width >= 1070px）
+      1070: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      // 画面幅が 1290px 以上の場合（window width >= 1290px）
+      1290: {
+        slidesPerView: 2.5,
+        spaceBetween: 45,
+      },
+      // 画面幅が 1290px 以上の場合（window width >= 1290px）
+      1550: {
+        slidesPerView: 3,
+        spaceBetween: 45,
+      }
     },
-    // 画面幅が 1070px 以上の場合（window width >= 1070px）
-    1070: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+    // pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
-    // 画面幅が 1290px 以上の場合（window width >= 1290px）
-    1290: {
-      slidesPerView: 2.5,
-      spaceBetween: 45,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    // 画面幅が 1290px 以上の場合（window width >= 1290px）
-    1550: {
-      slidesPerView: 3,
-      spaceBetween: 45,
-    }
-  },
-  // pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  });
 });
