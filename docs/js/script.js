f8f7c5bcac6aff4b07fae1ab88231db0f465efeb
y724u <68385107+y724u@users.noreply.github.com>
2021-02-06 20:45:50 +0900

    ポインター、テキストを変更

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,26 +4,26 @@ $(function () {
     let speed = 500;
     let href = $(this).attr('href');
     let target = $(href == '#' || href == '' ? 'html' : href);
-    let position = target.offset().top -100;
+    let position = target.offset().top - 100;
     $('html, body').animate({ scrollTop: position }, speed, 'swing');
     return false;
   });
 
-// // <!-- サービスシステム開発モーダル -->
-let scrollPosition1;
-$('.js-modalOpen').on('click', function () {
-  const target = $(this).data('target');
-  const modal = document.getElementById(target);
-  scrollPosition1 = $(window).scrollTop();
-  $(modal).addClass('show_modal');
-  $('body').addClass('fixed').css({ 'top': -scrollPosition1 });
-  return false;
-});
-$('.js-modalClose').on('click', function () {
-  $('.js-modal').removeClass('show_modal');
-  $('body').removeClass('fixed').css({ 'top': 0 });
-  window.scrollTo(0, scrollPosition1);
-});
+  // <!-- サービスシステム開発モーダル -->
+  let scrollPosition;
+  $('.js-modalOpen').on('click', function () {
+    const target = $(this).data('target');
+    const modal = document.getElementById(target);
+    scrollPosition = $(window).scrollTop();
+    $(modal).addClass('show_modal');
+    $('body').addClass('fixed').css({ 'top': -scrollPosition });
+    return false;
+  });
+  $('.js-modalClose').on('click', function () {
+    $('.js-modal').removeClass('show_modal');
+    $('body').removeClass('fixed').css({ 'top': 0 });
+    window.scrollTo(0, scrollPosition);
+  });
 
   // <!-- Slider  -->
   const swiper = new Swiper('.swiper-container', {
