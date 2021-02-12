2dcdd9158b6dbcdd3143984e40053d58b79c67aa
y724u <68385107+y724u@users.noreply.github.com>
2021-02-12 16:12:12 +0900

    カーソルを追加

--- docs/js/script.js
+++ docs/js/script.js
@@ -91,4 +91,57 @@ $(function () {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  // <!-- カーソルアニメーション -->
+  const
+    cursor = document.getElementById('js-cursor'),
+    chaser = document.getElementById('js-chaser'),
+    target = document.querySelector('a');
+  let
+    delay = 10,
+    cursorPosX = 0,
+    cursorPosY = 0,
+    chaserPosX = 0,
+    chaserPosY = 0;
+
+  // カーソルの遅延アニメーション
+  TweenMax.to({}, .001, {
+    repeat: -1,
+    onRepeat: function () {
+      chaserPosX += (cursorPosX - chaserPosX) / delay;
+      chaserPosY += (cursorPosY - chaserPosY) / delay;
+
+      TweenMax.set(cursor, {
+        css: {
+          left: cursorPosX - (cursor.clientWidth / 2),
+          top: cursorPosY - (cursor.clientWidth / 2)
+        }
+      });
+
+      TweenMax.set(chaser, {
+        css: {
+          left: chaserPosX - (chaser.clientWidth / 2),
+          top: chaserPosY - (chaser.clientWidth / 2)
+        }
+      });
+    }
+  });
+
+  // マウス座標を取得
+  document.onmousemove = function (event) {
+    cursorPosX = event.pageX;
+    cursorPosY = event.pageY;
+  };
+
+  // マウスオーバー時の処理
+  target.onmouseover = function () {
+    cursor.classList.add('is-active');
+    chaser.classList.add('is-active');
+  };
+
+  // マウスアウト時の処理
+  target.onmouseout = function () {
+    cursor.classList.remove('is-active');
+    chaser.classList.remove('is-active');
+  };
 });
