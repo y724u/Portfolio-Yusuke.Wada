d1ef5de58f52739c83a76278bf6d02ddefb695ee
y724u <68385107+y724u@users.noreply.github.com>
2021-02-27 18:33:42 +0900

    modalスピード変更

--- docs/js/script.js
+++ docs/js/script.js
@@ -46,7 +46,7 @@ $(function () {
     setTimeout(function () {
       scrollPosition = $(window).scrollTop();
       $('body').addClass('fixed').css({ 'top': -scrollPosition });
-    }, 200);
+    }, 400);
   });
   $(".js-modalClose").on("click", function () {
     $('body').removeClass('fixed').css({ 'top': 0 });
