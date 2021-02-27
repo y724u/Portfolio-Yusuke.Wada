c473184e8edfa5298e31f9af6328978e09cb90e1
y724u <68385107+y724u@users.noreply.github.com>
2021-02-27 18:19:39 +0900

    modaltimeoutを修正

--- docs/js/script.js
+++ docs/js/script.js
@@ -46,7 +46,7 @@ $(function () {
     setTimeout(function () {
       scrollPosition = $(window).scrollTop();
       $('body').addClass('fixed').css({ 'top': -scrollPosition });
-    }, 400);
+    }, 200);
   });
   $(".js-modalClose").on("click", function () {
     $('body').removeClass('fixed').css({ 'top': 0 });
